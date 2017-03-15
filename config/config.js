fa72dcaaa4905002c8f512950e2bba07c9884ae1
Nicolas Machado <nicoma63@gmail.com>
2017-03-15 13:00:56 +0100

    travis integration

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
 exports.PORT = process.env.PORT || 8080;
 
 exports.FACEBOOKAUTH = {
-        'clientID'      : global.FB_CLIENT_ID || '858085050997039', // your App ID
-        'clientSecret'  : global.FB_CLIENT_SECRET || '592abccfb6f6d9b974a0537086d1c067', // your App Secret
-        'callbackURL'   : global.FB_CALLBACK_URL || 'http://localhost:8080/auth/facebook/callback'
+        'clientID'      : process.env.FB_CLIENT_ID || '858085050997039', // your App ID
+        'clientSecret'  : process.env.FB_CLIENT_SECRET || '592abccfb6f6d9b974a0537086d1c067', // your App Secret
+        'callbackURL'   : process.env.FB_CALLBACK_URL || 'http://localhost:8080/auth/facebook/callback'
     };
