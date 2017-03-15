75ca85c01c617ab8d7cdabe66cd071275bd6d7db
Nicolas Machado <nicoma63@gmail.com>
2017-03-15 11:56:51 +0100

    travis integration

---
+++ test/test-server.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server.js');
+
+const should = chai.should();
+const app = server.app;
+
+chai.use(chaiHttp);
+
+
+describe('root page', function() {
+  it('should return status 200', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+    });
+  });
+});
