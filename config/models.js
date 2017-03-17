db7ddf65a87427f4d5aadfc385a2c3352b22ae98
Nicolas Machado <nicoma63@gmail.com>
2017-03-17 11:58:10 +0100

    Population implemented for users and requests

---
+++ config/models.js
@@ -0,0 +1,57 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// USER SCHEMA
+const UserSchema = mongoose.Schema({
+    username: { type: String, required: true },
+    authType: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    rating: { type: Number },
+    facebook: {
+        id: {type: String},
+        token: {type: String}
+    },
+    requests : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Request' }]
+});
+
+UserSchema.methods.apiRepr = function() {
+    return {
+        username: this.username || '',
+        email: this.email || '',
+        password: this.password || '',
+        id: this._id,
+        FBId: this.facebook.id || '',
+        FBToken: this.facebook.token || '',
+        authType: this.authType || '',
+        rating: this.rating || ''
+    };
+}
+
+UserSchema.methods.validatePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+}
+
+UserSchema.statics.hashPassword = function(password) {
+    return bcrypt.hash(password, 10);
+}
+
+// REQUEST SCHEMA
+const RequestSchema = mongoose.Schema({
+    datePosted: { type: Date, required: true },
+    dateEvent: { type: Date },
+    type: { type: String, required: true },
+    price: { type: Number },
+    rate: { type: String },
+    description: { type: String, required: true },
+    status: { type: String, required: true },
+    interested: { type: Array },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true  }
+});
+
+const User = mongoose.model('User', UserSchema);
+const Request = mongoose.model('Request', RequestSchema);
+
+module.exports = {Request, User}; 
