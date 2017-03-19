673dd67a54e5471db376dbe2fbd73b5e2a65ae36
Nicolas Machado <nicoma63@gmail.com>
2017-03-19 10:57:40 +0100

    hide config

--- config/config.js
+++ config/config.js
@@ -7,4 +7,4 @@ exports.FACEBOOKAUTH = {
         'clientID'      : process.env.FB_CLIENT_ID, // your App ID
         'clientSecret'  : process.env.FB_CLIENT_SECRET, // your App Secret
         'callbackURL'   : process.env.FB_CALLBACK_URL
-    };
+    }; 
